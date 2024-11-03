8c40a05cc1d6025e2adc76348ae223f2392c3d7b
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-11-03 09:05:44 +0530

    test: for root mod

--- spec/exercism_fetcher_spec.rb
+++ spec/exercism_fetcher_spec.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 RSpec.describe ExercismFetcher do
+  describe ".root" do
+    it "returns the root directory of the project" do
+      expected_root = File.dirname(File.dirname(__FILE__))
+      expect(ExercismFetcher.root).to eq(expected_root)
+    end
+  end
+
   it "has a version number" do
     expect(ExercismFetcher::VERSION).not_to be nil
   end
