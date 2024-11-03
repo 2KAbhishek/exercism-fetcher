22a0be23e25c32d1affaa3f12fdd758b76c52672
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-11-03 06:47:29 +0530

    feat: update main mod

--- lib/exercism_fetcher.rb
+++ lib/exercism_fetcher.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 require_relative "exercism_fetcher/version"
+require_relative "exercism_fetcher/fetcher"
+require_relative "exercism_fetcher/cli"
 
+# Main module
 module ExercismFetcher
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.root
+    File.dirname __dir__
+  end
 end
